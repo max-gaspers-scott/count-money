f3e0904c7452204cf8d427fe31b43858ae820105
maximillian gaspers scott <gaspersscottma@gmail.com>
2026-01-02 09:50:28 -0500

    finight presision math still not working

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ function App() {
   const [numbers, setNumbers] = useState([]);
   const [total, setTotal] = useState(0);
 
+  const [isBills, setIsBills] = useState(true);
+
 
   let add_id = 0;
+  let dig_one = {been_set: false, num:0};
+  let dig_two = {been_set: false, num:0};
 
   let add_one = () => {
     setNumbers(
@@ -17,12 +21,48 @@ function App() {
     );
   }
 
+  let add_100 = () => {
+    setNumbers(
+      [
+        ...numbers,
+        {id: add_id++, number: 100}
+      ]
+    )
+  }
+
+  let set_dig = (num) => {
+    let big_moltiple = 0;
+    let little_moltiple = 0;
+    if (isBills) {
+      big_moltiple = 10;
+      little_moltiple = 1;
+    }else {
+      big_moltiple = 0.1;
+      little_moltiple = 0.01;
+    }
+
+    if(dig_one.been_set){
+      dig_two.number = num;
+      console.log(dig_two);
+      setNumbers(
+        [
+          ...numbers,
+          {id: add_id++, number: (dig_one.number * big_moltiple + dig_two.number * little_moltiple)}
+        ]
+      )
+      dig_one.been_set = false;
+    }else {
+      dig_one.number = num;
+      dig_one.been_set = true;
+    }
+  }
+
   let sum = () => {
     let sum = 0;
     numbers.map(number => sum += number.number);
     return sum;
-
   }
+
   useEffect(
     () => {
       setTotal(sum());
@@ -30,8 +70,16 @@ function App() {
     }, [numbers]
   )
 
-  return (
 
+  let set_bills = () => {
+    setIsBills(true);
+  }
+
+  let set_coins = () => {
+    setIsBills(false);
+  }
+
+  return (
     <div className="App">
       {numbers.map(number => (
           <p key={number.id}>{number.number}</p>
@@ -39,14 +87,56 @@ function App() {
       <div className="container mx-auto p-4">
         {total}
       </div>
+
+      { isBills ? (
       <div className="mt-8 flex justify-around">
-          <button onClick={add_one} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            add
+          <button onClick={add_one} className="bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            $1
           </button>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Button
+          <button onClick={add_100} className="bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            $100
           </button>
+      </div> ) : (
+      <div className="mt-8 flex justify-around">
+          <button onClick={add_one} className="bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            $1
+          </button>
+      </div>
+      )
+      }
+
+      <div className="flex-row justify-around">
+        <div className="flex justify-around">
+          <button onClick={() => set_dig(1)} className="bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">1</button>
+          <button onClick={() => set_dig(2)} className="bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">2</button>
+          <button onClick={() => set_dig(3)} className="bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">3</button>
         </div>
+      </div>
+
+      <div className="flex-row justify-around">
+        <div className="flex justify-around">
+          <button onClick={() => set_dig(4)} className="bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">4</button>
+          <button onClick={() => set_dig(5)} className="bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">5</button>
+          <button onClick={() => set_dig(6)} className="bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">6</button>
+        </div>
+      </div>
+
+      <div className="flex-row justify-around">
+        <div className="flex justify-around">
+          <button onClick={() => set_dig(7)} className="bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">7</button>
+          <button onClick={() => set_dig(8)} className="bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">8</button>
+          <button onClick={() => set_dig(9)} className="bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">9</button>
+        </div>
+      </div>
+
+
+      <div className="flex justify-around">
+
+          <button onClick={set_bills} className="bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">bills</button>
+          <button onClick={set_coins} className="bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">coins</button>
+
+      </div>
+
    </div>
   );
 }
